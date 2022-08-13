866eda4c683e3573a005802f3142b227a1189bd1
SMShoron <shoronsrs@gmail.com>
2022-08-13 15:48:31 +0600

    Tweaking some stuffs

--- test/specs/FirstTest.js
+++ test/specs/FirstTest.js
@@ -1,13 +1,38 @@
-describe('This is my first test in WEBDIO', () => {
+// describe('This is my first test in WEBDIO', () => {
 
-    it('My first test case , Verify URL', () => {
+//     it('My first test case , Verify URL', () => {
 
-        browser.url('https://the-internet.herokuapp.com/login');
-        console.log('Session id is: ',browser.sessionId);
-        console.log('Capabilities: ',browser.capabilities);
-        
+//         browser.url('https://webdriver.io/')
+//         expect(browser).toHaveUrlContaining('sh')
+//     });
 
-        
+//     it('Verify Title' , () => {
+
+//          browser.url('https://the-internet.herokuapp.com/login')
+//          expect(browser).toHaveTitle('The Internet')
+
+//     });
+    
+// });
+
+describe('Verify Login',  () => {
+
+    it('THE INTERNET LOGIN' , async () => {
+
+         browser.url('https://the-internet.herokuapp.com/login')
+         await expect(browser).toHaveTitle('The Internet')
+
+         const username = await $("//input[@id='username']")
+         await username.setValue('tomsmith')
+
+
+         const password = await $("//input[@id='password']")
+         await password.setValue('SuperSecretPassword!')
+
+         const loginClick = await $("//i[contains(text(),'Login')]")
+         loginClick.click()
+
+
     });
     
 });
